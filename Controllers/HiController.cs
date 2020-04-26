92df739c9269030773340d2a2e701c7e08b1ca3a
Shaik Fayaz <2shaik.fayaz@gmail.com>
2020-04-26 20:36:44 +0530

    Add project files.

---
+++ Controllers/HiController.cs
@@ -0,0 +1,33 @@
+﻿using Activity1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity1.Controllers
+{
+    public class HiController : Controller
+    {
+        TrialdbEntities db = new TrialdbEntities();
+        [HttpGet]
+        // GET: Hi
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string tags)
+        {
+            Student c = db.Students.Where(x => x.Name.Equals(tags)).SingleOrDefault();
+            return View(c);
+        }
+        public JsonResult getcompanies()
+        {
+            List<Student> li = db.Students.OrderBy(x => x.Id).ToList();
+
+            return Json(li, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Activity1.MvcApplication" Language="C#" %>
